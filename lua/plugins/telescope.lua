968d3b0c178e45230f1e84a8a7eb12b3e482e20f
varenya <vare90@gmail.com>
2026-03-24 15:25:59 +0530

    keymaps for diagnostics

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,29 +1,39 @@
 return {
-  {
-    'nvim-telescope/telescope.nvim', version = '*',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      -- optional but recommended
-      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    },
-    opts = {
-      extensions = {
-        ["ui-select"] = {
-          require("telescope.themes").get_dropdown {
-          }
-        }
-      }
-    },
-    config = function()
-      local builtin = require("telescope.builtin")
-      vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-    end
-  },
-  {
-    'nvim-telescope/telescope-ui-select.nvim',
-    config = function()
-      require("telescope").load_extension("ui-select")
-    end
-  }
+	{
+		"nvim-telescope/telescope.nvim",
+		version = "*",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			-- optional but recommended
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		},
+		opts = {
+			extensions = {
+				["ui-select"] = {
+					require("telescope.themes").get_dropdown({}),
+				},
+			},
+		},
+		config = function()
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader>b", require("telescope.builtin").buffers, { desc = "[B]uffers" })
+			vim.keymap.set("n", "<leader>d", function()
+				builtin.diagnostics({
+					bufnr = 0, -- Limit to current buffer
+					-- Optional: additional configuration here
+				})
+			end, { desc = "Telescope Diagnostics (Current Buffer)" })
+
+			-- Optional: a keymap to send the diagnostics to the location list
+			vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Send Diagnostics to Location List" })
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,3 +3,15 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set relativenumber")
+vim.keymap.set("n", "<leader>e", function()
+	vim.diagnostic.open_float({ focusable = true })
+end, { desc = "Expand an Error into a float" })
+
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
+
+vim.diagnostic.config({
+	virtual_text = false, -- Disables inline text
+	signs = true, -- Shows icons in the sign column
+	underline = true, -- Underlines the problematic text
+})
